3a6dae902b8d65f7cd49323e17a62795df13d214
hansoloisthegoat <emailrepowizard@gmail.com>
2024-08-06 11:27:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os 
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return jsonify(message = 'Hello, World!')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',8080))
